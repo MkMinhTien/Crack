ed39dbd2eb33841a51d563f687ec676db284ebf6
MkMinhTien <60256636+MkMinhTien@users.noreply.github.com>
2020-01-30 13:32:36 +0700

    My NodeJS hack

--- node.js
+++ node.js
@@ -3,13 +3,17 @@ var app = express();
 
 var port = process.env.PORT || 3000
 
+app.use('/js', express.static(__dirname + '/config/script'));
+app.use('/css', express.static(__dirname + '/config/style'));
+app.use('/img', express.static(__dirname + '/config/image'));
+
 app.get('/', (req, res) =>
 
-    res.send('Welcome to Crack website')
+    res.sendFile(__dirname + '/public/server.html');
 
 )
 
 app.listen(port, () =>
-    console.log(`Website app listening on port ${port}!`)
+    console.log(`Website app listening on port ${port}!`);
 
 )
